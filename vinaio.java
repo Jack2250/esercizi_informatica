65a8759652c459e1f812e47da4c40121bfea0fb8
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2023-12-05 13:23:04 +0100

    Update vinaio.java

--- vinaio.java
+++ vinaio.java
@@ -1,81 +1,91 @@
 import java.util.Scanner;
 
-public class Main
-{
-  public static void main (String[] args)
-  {
-    int scelta;
-    double barbera = 1.5;
-    int lugana = 2;
-    int merlot = 1;
-    double iva = 0.21;
-    int bottiglione;
-    double capacita = 1.5;
-    float costolitro = 1;
-
-    Scanner keyBoard = new Scanner (System.in);
-      System.out.println ("Inserisci il numero di bottiglioni");
-      bottiglione = keyBoard.nextInt ();
-      System.out.println ("Hai acquistato " + bottiglione + "bottiglione/i");
-
-      System.out.println
-      ("Quale tipo di vino vuoi acquistare? 1 barbera, 2 lugana, 3 merlot");
-      scelta = keyBoard.nextInt ();
-    if (scelta == 1)
-      {
-
-	double litriAcquistati = bottiglione * capacita;
-	  System.out.println ("Hai acquistato " + litriAcquistati + "litri");
-
-	double prezzoTotale = barbera * litriAcquistati;
-	  System.out.println ("Hai speso " + prezzoTotale + "$");
-
-	double prezzoIva = prezzoTotale * iva;
-	  System.out.println ("Prezzo di cui iva " + prezzoIva + "$");
-
-	double prezzoTotaleConIva = prezzoTotale + prezzoIva;
-	  System.out.println ("Prezzo totale con iva " + prezzoTotaleConIva +
-			      "$");
+public class Main {
+    public static void main(String[] args) {
+        int scelta;
+        double barbera = 1.5;
+        int lugana = 2;
+        int merlot = 1;
+        double iva = 0.21;
+        int bottiglione = 0;
+        double capacita = 1.5;
+        float costolitro = 1;
+        double litriAcquistati = bottiglione * capacita;
+        double prezzoTotale = 0;
+        double prezzoIva = prezzoTotale * iva;
+        double prezzoTotaleConIva = prezzoTotale + prezzoIva;
 
 
-      }
-    else if (scelta == 2)
-      {
-	double litriAcquistati = bottiglione * capacita;
-	System.out.println ("Hai acquistato " + litriAcquistati + "litri");
+        Scanner keyBoard = new Scanner(System.in);
+        System.out.println("Inserisci il numero di bottiglioni");
+        bottiglione = keyBoard.nextInt();
+        do {
+            ClearScr();
+            System.out.println("Numero bottiglioni non valido, reinserisci il numero di bottiglioni");
+            bottiglione = keyBoard.nextInt();
 
-	double prezzoTotale = lugana * litriAcquistati;
-	System.out.println ("Hai speso " + prezzoTotale + "$");
+        } while (bottiglione< 0);
+        System.out.println("Hai acquistato " + bottiglione + "bottiglione/i");
 
-	double prezzoIva = prezzoTotale * iva;
-	System.out.println ("Prezzo di cui iva " + prezzoIva + "$");
+        System.out.println
+                ("Quale tipo di vino vuoi acquistare? 1 barbera, 2 lugana, 3 merlot");
+        scelta = keyBoard.nextInt();
+        if (scelta == 1) {
 
-	double prezzoTotaleConIva = prezzoTotale + prezzoIva;
-	System.out.println ("Prezzo totale con iva " + prezzoTotaleConIva +
-			    "$");
 
-      }
-    else if (scelta == 3)
-      {
-	double litriAcquistati = bottiglione * capacita;
-	System.out.println ("Hai acquistato " + litriAcquistati + "litri");
+            System.out.println("Hai acquistato " + litriAcquistati + "litri");
 
-	double prezzoTotale = merlot * litriAcquistati;
-	System.out.println ("Hai speso " + prezzoTotale + "$");
+            prezzoTotale = barbera * litriAcquistati;
+            System.out.println("Hai speso " + prezzoTotale + "$");
 
-	double prezzoIva = prezzoTotale * iva;
-	System.out.println ("Prezzo di cui iva " + prezzoIva + "$");
 
-	double prezzoTotaleConIva = prezzoTotale + prezzoIva;
-	System.out.println ("Prezzo totale con iva " + prezzoTotaleConIva +
-			    "$");
+            System.out.println("Prezzo di cui iva " + prezzoIva + "$");
 
-      }
-    else
-      {
-	System.out.println ("Scelta non valida");
-      }
 
-  }
+            System.out.println("Prezzo totale con iva " + prezzoTotaleConIva +
+                    "$");
+
+
+        } else if (scelta == 2) {
+
+            System.out.println("Hai acquistato " + litriAcquistati + "litri");
+
+            prezzoTotale = lugana * litriAcquistati;
+            System.out.println("Hai speso " + prezzoTotale + "$");
+
+
+            System.out.println("Prezzo di cui iva " + prezzoIva + "$");
+
+
+            System.out.println("Prezzo totale con iva " + prezzoTotaleConIva +
+                    "$");
+
+        } else if (scelta == 3) {
+
+            System.out.println("Hai acquistato " + litriAcquistati + "litri");
+
+            prezzoTotale = merlot * litriAcquistati;
+            System.out.println("Hai speso " + prezzoTotale + "$");
+
+
+            System.out.println("Prezzo di cui iva " + prezzoIva + "$");
+
+
+            System.out.println("Prezzo totale con iva " + prezzoTotaleConIva +
+                    "$");
+
+        } else {
+            System.out.println("Scelta non valida");
+        }
+
+    }
+
+    static void ClearScr() {
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+
+        } catch (Exception e) {
+            System.out.println("errore" + e);
+        }
+    }
 }
-
