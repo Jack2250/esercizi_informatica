eed1b43a51779329f0150cfcd824d1601acaf175
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2024-01-17 11:16:14 +0100

    Update appNumeri.java

--- appNumeri.java
+++ appNumeri.java
@@ -3,61 +3,70 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int opzione;
-        int n, casuale;
-        boolean fine = true;
+
         Scanner tastiera = new Scanner(System.in);
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        
+        int opzioni, quantiNumeri = 0, nPari, posizione = 0;
+        boolean fine = true;
+        int[] vetNumeri = null;
+        int[] vetPari = null;
+
         do {
             ClrScr();
             menu();
             System.out.println("seleziona un'opzione");
-            opzione = tastiera.nextInt();
+            opzioni = tastiera.nextInt();
+            switch (opzioni) {
 
-            switch (opzione) {
                 case 1:
-                    System.out.println("genera numeri");
-                    System.out.println("Quanti numeri casuali vuoi generare?");
-                    n = scanner.nextInt();
-                    int[] vettore = caricaVettore(n);
-                    int[] vettore1;
-                    vettore1 = vettore;
-                    for (int i = 0; i < vettore.length; i++) {
-                        System.out.println(vettore[i]);
-                    }
-                    System.out.println("----------------------------------------");
-                    for (int i = 0; i < vettore1.length; i++) {
-                        System.out.println(vettore1[i]);
-                    }
+                    System.out.println(" quanti numeri vuoi generare?");
+                    quantiNumeri = tastiera.nextInt();
+                    vetNumeri = numeriCasuali(quantiNumeri);
                     break;
                 case 2:
                     System.out.println("visualizza numeri");
+                    if (vetNumeri != null)
+                        visualizza(vetNumeri);
+                    else System.out.println("non è stato creato il vettore");
                     break;
                 case 3:
-                    System.out.println("app numeri pari");
+                    System.out.println(" numeri pari");
+                    if (vetNumeri != null) {
+                        vetPari = new int[quantiNumeri];
+                        for (int i = 0; i < quantiNumeri; i++) {
+                            if (vetNumeri[i] % 2 == 0) {
+                                vetPari[posizione] = vetNumeri[i];
+                                posizione = posizione + 1;
+                            }
+
+                        }
+                    } else System.out.println("non è stato creato il vettore");
                     break;
                 case 4:
-                    System.out.println("exit");
+                    if (vetPari != null)
+                        visualizza(vetPari);
+                    else System.out.println("non è stato creato il vettore");
+                    break;
+                case 5:
+                    System.out.println(" exit");
                     fine = false;
                     break;
                 default:
                     System.out.println("opzione errata");
-                    break;
             }
             Wait(2000);
 
         } while (fine);
-        
+
+
     }
 
     private static void menu() {
         System.out.println("== app numeri casuali");
         System.out.println("[1] genera numeri");
         System.out.println("[2] visualizza numeri");
-        System.out.println("[3] app numeri pari");
-        System.out.println("[4] exit");
+        System.out.println("[3] numeri pari");
+        System.out.println("[4] visualizza pari");
+        System.out.println("[5] exit");
     }
 
     private static void ClrScr() {
@@ -76,14 +85,21 @@ public class Main {
         }
     }
 
-    public static int[] caricaVettore(int n) {
-        Random random = new Random();
+    private static int[] numeriCasuali(int n) {
         int casuale;
         int[] vettore = new int[n];
+        Random rn = new Random();
         for (int i = 0; i < n; i++) {
-            casuale = random.nextInt(1, 101);
+            casuale = rn.nextInt(1, 101);
             vettore[i] = casuale;
         }
         return vettore;
+    }
+
+    private static void visualizza(int[] v) {
+        for (int i = 0; i < v.length; i++) {
+            System.out.println(i + "=" + v[i]);
+        }
+
     }
 }
