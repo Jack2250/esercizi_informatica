878696fe08f7fa912d04caf2ebf165c62738f90d
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2024-01-17 10:25:50 +0100

    Update appNumeri.java

--- appNumeri.java
+++ appNumeri.java
@@ -5,20 +5,20 @@ public class Main {
     public static void main(String[] args) {
         int opzione;
         int n, casuale;
-        boolean fine=true;
+        boolean fine = true;
         Scanner tastiera = new Scanner(System.in);
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-
-
-        do{
+        
+        do {
             ClrScr();
             menu();
-            System.out.println("seleziona un'opzione" );
+            System.out.println("seleziona un'opzione");
             opzione = tastiera.nextInt();
 
-            switch(opzione){
-                case 1: System.out.println("genera numeri");
+            switch (opzione) {
+                case 1:
+                    System.out.println("genera numeri");
                     System.out.println("Quanti numeri casuali vuoi generare?");
                     n = scanner.nextInt();
                     int[] vettore = caricaVettore(n);
@@ -31,36 +31,36 @@ public class Main {
                     for (int i = 0; i < vettore1.length; i++) {
                         System.out.println(vettore1[i]);
                     }
-                break;
-                case 2: System.out.println("visualizza numeri");
-                break;
-                case 3: System.out.println("app numeri pari");
-                break;
-                case 4: System.out.println("exit");
-                fine=false;
-                break;
-                default: System.out.println("opzione errata");
-                break;
-            }Wait(2000);
+                    break;
+                case 2:
+                    System.out.println("visualizza numeri");
+                    break;
+                case 3:
+                    System.out.println("app numeri pari");
+                    break;
+                case 4:
+                    System.out.println("exit");
+                    fine = false;
+                    break;
+                default:
+                    System.out.println("opzione errata");
+                    break;
+            }
+            Wait(2000);
 
-        }while(fine);
-
-
-
-
-
-
-
-
+        } while (fine);
+        
     }
-    private static void menu(){
+
+    private static void menu() {
         System.out.println("== app numeri casuali");
         System.out.println("[1] genera numeri");
         System.out.println("[2] visualizza numeri");
         System.out.println("[3] app numeri pari");
         System.out.println("[4] exit");
     }
-    private static void ClrScr(){
+
+    private static void ClrScr() {
         try {
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         } catch (Exception e) {
@@ -69,12 +69,13 @@ public class Main {
     }
 
     private static void Wait(int tempo) {
-        try{
+        try {
             Thread.sleep(tempo);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+
     public static int[] caricaVettore(int n) {
         Random random = new Random();
         int casuale;
